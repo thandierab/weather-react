8eabdd6c2a824dab26aec650332c22f475abb8ab
thandierab <thandierabasimane@gmail.com>
2024-09-21 19:36:29 +0200

    Initial commit

---
+++ src/Weather.js
@@ -0,0 +1,19 @@
+import React from "react";
+import axios from "axios";
+import { Puff } from "react-loader-spinner";
+
+export default function Weather(props) {
+  function handleResponse(response) {
+    alert(
+      `The weather in ${response.data.name} is ${response.data.main.temp}°C`
+    );
+  }
+
+  let apiKey = "97c2f6a3b34509ac62090edc5d18d949";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${props.city}&appid=${apiKey}&units=metric`;
+
+  axios.get(apiUrl).then(handleResponse);
+  return (
+    <Puff type="Puff" color="#00BFFF" height={100} width={100} timeout={3000} />
+  );
+}
